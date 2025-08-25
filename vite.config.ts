4a4f503c95f4f2854d77ba4d241c44a67a8d6ef6
lehoangtrung2209 <33251852+lehoangtrung2209@users.noreply.github.com>
2025-08-25 20:16:26 +0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // nếu dùng React
-
-export default defineConfig({
-  base: '/happy-birthday-ha/', // Tên repo trên GitHub
-  plugins: [react()],
-})
